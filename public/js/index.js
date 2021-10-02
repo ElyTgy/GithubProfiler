2da87af88f6c5b9d60ae2b3bc422a45776667b9a
Ellie <ytaghavi@yahoo.com>
2021-10-02 10:33:13 -0700

    Cooler Landing Page

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,110 @@
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.validate')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+particlesJS("particles-js", {
+        particles: {
+          number: {
+            value: 19,
+            density: {
+              enable: true,
+              value_area: 481.0236182596568,
+            },
+          },
+          color: {
+            value: "#75D5FD",
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#ffffff",
+            },
+            polygon: {
+              nb_sides: 12,
+            },
+            image: {
+              src: "https://www.sprinklr.com/wp-content/themes/sprinklr/assets/images/logo_sprinklr.svg",
+              width: 250,
+              height: 100,
+            },
+          },
+          opacity: {
+            value: 0.3286994724774322,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false,
+            },
+          },
+          size: {
+            value: 47.34885849793636,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 9.59040959040959,
+              size_min: 5.594405594405594,
+              sync: true,
+            },
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 6.413648243462092,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "bounce",
+            bounce: false,
+            attract: {
+              enable: true,
+              rotateX: 320.6824121731046,
+              rotateY: 481.0236182596568,
+            },
+          },
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: {
+              enable: true,
+              mode: "repulse",
+            },
+            onclick: {
+              enable: true,
+              mode: "push",
+            },
+            resize: true,
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              line_linked: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 400,
+              size: 40,
+              duration: 2,
+              opacity: 8,
+              speed: 3,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+            push: {
+              particles_nb: 4,
+            },
+            remove: {
+              particles_nb: 2,
+            },
+          },
+        },
+        retina_detect: true,
+      });
